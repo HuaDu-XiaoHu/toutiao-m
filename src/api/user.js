5bbb3ebf0478b07c0a2735d89b0f76d664f94c99
HuQiQi <2206778929@qq.com>
2020-05-12 22:58:58 +0800

    我的-使用请求拦截器设置tolen

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,6 @@
 // 用户相关请求模块
 import request from '@/utils/request'
-import store from '@/store/'
+// import store from '@/store/'
 // 登录/注册
 export const login = data => {
   return request({
@@ -20,10 +20,10 @@ export const sendSms = mobile => {
 export const getCurrentUser = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
-    headers: {
-      Authorization: `Bearer ${store.state.user.token
-        }`
-    }
+    url: '/app/v1_0/user'
+    // headers: {
+    //   Authorization: `Bearer ${store.state.user.token
+    //     }`
+    // }
   })
 }
