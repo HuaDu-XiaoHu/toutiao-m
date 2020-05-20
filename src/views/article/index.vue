4e59d17a36b9ac102e975fd7bff1d5d79ffb58d7
HuQiQi <2206778929@qq.com>
2020-05-21 00:41:30 +0800

    文章详情-创建组件并配置路由

---
+++ src/views/article/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="article-container">文章</div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleIndex',
+  components: {},
+  props: {
+    articleId: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  watch: {},
+  created () { },
+  mounted () { },
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
